2005762148e5bc3d1459acac5a39439532a5a05a
Mark Allen R. Alberto <allen.alberto@academy.opswerks.com>
2025-01-07 14:45:45 +0800

    Restructure Files

--- scripts/teamcity/teamcity-agent-docker.sh
+++ scripts/teamcity/teamcity-agent-docker.sh
@@ -2,6 +2,8 @@ AGENT_NAME=$1
 
 docker run --name=$AGENT_NAME \
 -v team_city_agent_config-$AGENT_NAME:/data/teamcity_agent/conf \
--e SERVER_URL="http://172.17.0.3:8111" \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v /usr/local/bin/docker:/usr/bin/docker \
+-e SERVER_URL="http://172.17.0.2:8111" \
 -e AGENT_NAME=$AGENT_NAME \
 -d jetbrains/teamcity-agent
