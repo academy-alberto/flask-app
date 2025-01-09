e2e29c3a3cce69d35b3aaf5f35f78ec30f7acaa4
academy-alberto <allen.alberto@academy.opswerks.com>
2025-01-09 20:41:32 +0800

    Moved the Scripts into the Folder

---
+++ scripts/teamcity-setup/teamcity-install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f ../../manifest/teamcity/teamcity-ns.yml
+kubectl apply -f ../../manifest/teamcity/.
--- scripts/teamcity/teamcity-server.sh
+++
@@ -1,5 +0,0 @@
-docker run \
--v team_city_data:/data/teamcity_server/datadir \
--v team_city_logs:/opt/teamcity/logs \
--p 80:8111 \
--d jetbrains/teamcity-server
